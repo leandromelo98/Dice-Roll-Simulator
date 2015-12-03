b91b0f5e1dd01de3a5e6b9dc68dba3f6f1a21785
leandromelo98 <leandromelo@seoulchristian.org>
2015-12-03 15:45:12 +0900

    Update dice roll.py

--- dice roll.py
+++ dice roll.py
@@ -1,9 +1,15 @@
+#while true: makes it keep going
 while True:
+#import random: generate random numbers        
         import random
+#print: is what i want to display        
         print("DICE ROLL SIMULATOR")
         print("press enter")
+#cmd: what you want the computer to do after you press those keys.     
         cmd = raw_input("Enter to roll, e to quit")
+#number=random.randint: chose random number between 1,6.        
         number=random.randint(1,6)
+#if number 1 is picked you should be able to see the figure at the bottom        
         if number==1:
                 print("[--------]")
                 print("[        ]")
@@ -40,3 +46,4 @@ while True:
                 print("[ 0    0 ]")
                 print("[ 0    0 ]")
                 print("[--------]")
+
