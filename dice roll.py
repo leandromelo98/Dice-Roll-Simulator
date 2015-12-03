60dc01c23a03408796cca8f40c74ac6d1d6bf3d4
leandro melo <leandro melo>
2015-12-03 15:28:25 +0900

    dice roll

---
+++ dice roll.py
@@ -0,0 +1,42 @@
+while True:
+        import random
+        print("DICE ROLL SIMULATOR")
+        print("press enter")
+        cmd = raw_input("Enter to roll, e to quit")
+        number=random.randint(1,6)
+        if number==1:
+                print("[--------]")
+                print("[        ]")
+                print("[    0   ]")
+                print("[        ]")
+                print("[--------]")
+        if number==2:
+                print("[--------]")
+                print("[        ]")
+                print("[ 0    0 ]")
+                print("[        ]")
+                print("[--------]")
+        if number==3:
+                print("[--------]")
+                print("[0       ]")
+                print("[   0    ]")
+                print("[      0 ]")
+                print("[--------]")
+        if number==3:
+                print("[--------]")
+                print("[ 0    0 ]")
+                print("[        ]")
+                print("[ 0    0 ]")
+                print("[--------]")
+        if number==5:
+                print("[--------]")
+                print("[ 0     0]")
+                print("[    0   ]")
+                print("[ 0     0]")
+                print("[--------]")
+        if number==6:
+                print("[--------]")
+                print("[ 0    0 ]")
+                print("[ 0    0 ]")
+                print("[ 0    0 ]")
+                print("[--------]")
