6eb78dd43f9c71182f3917c579a9eee033cb3dfd
leandro melo <leandro melo>
2016-01-04 14:38:28 -0200

    dice

--- dice roll.py
+++ dice roll.py
@@ -1,16 +1,17 @@
-
-# while true: makes it keep going
+#This progrmam will simulate a dice
+#while true: makes it keep rolling the dice.
 while True:
-# import random: generate random numbers
+# import random: generate random numbers.
     import random
-# print: is what i want to display
+# print: is what i want to display for the user to see
     print ("DICE ROLL SIMULATOR")
     print ("press enter")
+
 # cmd: what you want the computer to do after you press those keys.
-    cmd = raw_input ("Enter to run, e to quit")
+    cmd = raw_input ("Enter to run")
 # number=random.randint: chose random number between 1,6.
     number=random.randint(1,6)
-# if number 1 is picked you should be able to see the figure at the bottom
+# if number 1 or any other numer is picked you should be able to see the figure at the bottom.
     if number==1:
         print("[----------]")
         print("[          ]")
