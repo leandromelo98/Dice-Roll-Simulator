4fcd0c76903d9e49afdaf9f2a655ea38233787cb
leandro melo <leandro melo>
2016-01-21 15:22:50 +0900

    fixed erros

--- dice roll.py
+++ dice roll.py
@@ -1,50 +1,72 @@
 #This progrmam will simulate a dice
-#while true: makes it keep rolling the dice.
-while True:
-# import random: generate random numbers.
-    import random
-# print: is what i want to display for the user to see
-    print ("DICE ROLL SIMULATOR")
-    print ("press enter")
 
-# cmd: what you want the computer to do after you press those keys.
-    cmd = raw_input ("Enter to run")
-# number=random.randint: chose random number between 1,6.
-    number=random.randint(1,6)
-# if number 1 or any other numer is picked you should be able to see the figure at the bottom.
+# allow the to receive strins from the user
+import string
+#Allows you to exit the program
+from sys import exit
+# impolrt random: generate random numbers.
+from random import randint
+
+
+# It will run the dice
+def runDice():
+    # number=random.randint: chose random number between 1,6.
+    number=randint(1,6)
+
+    # if number 1 or any other numer is picked you should be able to see the figure at the bottom.
     if number==1:
         print("[----------]")
         print("[          ]")
         print("[     O    ]")
         print("[          ]")
         print("[----------]")
-    if number==2:
+    elif number==2:
         print("[----------]")
         print("[          ]")
         print("[  O    O  ]")
         print("[          ]")
         print("[----------]")
-    if number==3:
+    elif number==3:
         print("[----------]")
         print("[  O       ]")
         print("[    O     ]")
         print("[       O  ]")
         print("[----------]")
-    if number==4:
+    elif number==4:
         print("[----------]")
         print("[   O   O  ]")
         print("[          ]")
         print("[   O   O  ]")
         print("[----------]")
-    if number==5:
+    elif number==5:
         print("[----------]")
         print("[  O    O  ]")
         print("[     O    ]")
         print("[  O     O ]")
         print("[----------]")
-    if number==6:
+    elif number==6:
         print("[----------]")
         print("[   O   O  ]")
         print("[   O   O  ]")
         print("[   O   O  ]")
         print("[----------]")
+    else:
+        sys.exit()
+
+def runProgram():
+#This is what the user will see
+    print "Run the program? [y/n]\n:"
+    cmd = raw_input()
+    if cmd == "n":
+        print "Bye"
+        exit()
+    elif cmd =='y':
+        runDice()
+    else:
+        runProgram()
+
+#while true: makes it keep rolling the dice.
+while True:
+# print: is what i want to display for the user to see
+    print ("DICE ROLL SIMULATOR")
+    runProgram()
